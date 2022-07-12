c38e81de2cb5c0c75671262f036491f5867fded4
Jcupzz <jcupzz255@gmail.com>
2022-07-12 20:15:30 +0530

    Error Fixed : MinSDKVersion updated to 24 Android Nougat Pending Error : MissingPluginException(No implementation found for method loadModel on channel tflite)

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:live_emotion/main.dart';
 import 'package:tflite/tflite.dart';
 
-
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -24,73 +22,74 @@ class _HomeState extends State<Home> {
     loadmodel();
   }
 
-  loadCamera(){
+  loadCamera() {
     cameraController = CameraController(cameras![0], ResolutionPreset.medium);
     cameraController!.initialize().then((value) {
-      if(!mounted){
+      if (!mounted) {
         return;
-      }
-      else{
+      } else {
         setState(() {
           cameraController!.startImageStream((imageStream) {
-          cameraImage = imageStream;
-          runModel();
+            cameraImage = imageStream;
+            runModel();
           });
         });
       }
     });
   }
-  runModel()async{
-    if(cameraImage!=null){
-    var predictions = await Tflite.runModelOnFrame(
-      bytesList: cameraImage!.planes.map((plane) {
-      return plane.bytes;
-    }).toList(),
-    imageHeight: cameraImage!.height,
-    imageWidth: cameraImage!.width,
-    imageMean: 127.5,
-    imageStd: 127.5,
-    rotation: 90,
-    numResults: 2,
-    threshold: 0.1,
-    asynch: true
-   );
-   predictions!.forEach((element) {
-    setState(() {
-      output = element['label'];
-    });
-   });
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage!.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: cameraImage!.height,
+          imageWidth: cameraImage!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true);
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+        });
+      });
     }
-    }
-  
-  loadmodel()async{
-    await Tflite.loadModel( model: 'assets/model.tflite',labels: 'assets/label.txt');
   }
 
-  
+  loadmodel() async {
+    await Tflite.loadModel(
+        model: 'assets/model.tflite', labels: 'assets/label.txt');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Live Emotion Detection App"),),
-        body: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(20),
-            child: Container(
-              height: MediaQuery.of(context).size.height*0.7,
-              width: MediaQuery.of(context).size.width,
-              child: !cameraController!.value.isInitialized?
-              Container():
-              AspectRatio(aspectRatio: cameraController!.value.aspectRatio,
-              child: CameraPreview(cameraController!)
-            ),)
-        ),
-        Text(output,
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,fontSize: 20
-        ),),
+        title: const Text("Live Emotion Detection App"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.7,
+                width: MediaQuery.of(context).size.width,
+                child: !cameraController!.value.isInitialized
+                    ? Container()
+                    : AspectRatio(
+                        aspectRatio: cameraController!.value.aspectRatio,
+                        child: CameraPreview(cameraController!)),
+              )),
+          Text(
+            output,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
         ],
-        ),
+      ),
     );
   }
 }
